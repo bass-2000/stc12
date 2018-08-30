de190ab3a37e099a73b9a937f1fe112c0b40709c
bass-2000 <bass-2000@yandex.ru>
2018-08-30 14:23:19 +0300

    2 tests from 05-io

--- stc12-05-io/src/test/java/ru/innopolis/stc12/io/EmployeeDataTest.java
+++ stc12-05-io/src/test/java/ru/innopolis/stc12/io/EmployeeDataTest.java
@@ -1,47 +1,33 @@
 package ru.innopolis.stc12.io;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 
 public class EmployeeDataTest {
 
     @Test
     public void readListFromFile() {
+        Employee employee1 = new Employee("Alex", 28, 98000, Job.ENGENEER);
+        EmployeeData.clearFile();
+        EmployeeData.save(employee1);
+        ArrayList<Employee> list = EmployeeData.readListFromFile();
+        Assert.assertEquals(employee1,list.get(0));
     }
 
     @Test
     public void save() {
-    }
+        EmployeeData.clearFile();
+        Employee employee1 = new Employee("Alex", 28, 98000, Job.ENGENEER);
+        Employee employee2 = new Employee("Fill", 29, 90800, Job.ADMINISTRATOR);
+        Assert.assertTrue(EmployeeData.save(employee1));
+        Assert.assertTrue(EmployeeData.save(employee2));
+        ArrayList<Employee> list = new ArrayList<>();
+        list.add(employee1);
+        list.add(employee2);
+        Assert.assertEquals(list, EmployeeData.readListFromFile());
 
-    @Test
-    public void delete() {
-    }
-
-    @Test
-    public void getByName() {
-    }
-
-    @Test
-    public void clearFile() {
-    }
-
-    @Test
-    public void getByJob() {
-    }
-
-    @Test
-    public void saveOrUpdate() {
-    }
-
-    @Test
-    public void changeAllWork() {
-    }
-
-    @Test
-    public void printFile() {
-    }
-
-    @Test
-    public void setFileName() {
     }
 }
