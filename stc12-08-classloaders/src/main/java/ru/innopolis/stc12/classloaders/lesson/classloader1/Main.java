aa1d487e6324b19292ee970a57d1e062d0eab2e3
bass-2000 <bass-2000@yandex.ru>
2018-09-04 08:19:41 +0300

    initial commit

---
+++ stc12-08-classloaders/src/main/java/ru/innopolis/stc12/classloaders/lesson/classloader1/Main.java
@@ -0,0 +1,4 @@
+package ru.innopolis.stc12.classloaders.lesson.classloader1;
+
+public class Main {
+}
---
+++ stc12-08-classloaders/src/main/java/ru/innopolis/stc12/classloaders/lesson/classloader2/Main.java
@@ -0,0 +1,4 @@
+package ru.innopolis.stc12.classloaders.lesson.classloader2;
+
+public class Main {
+}
