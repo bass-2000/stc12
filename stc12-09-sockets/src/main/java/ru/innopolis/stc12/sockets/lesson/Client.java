334ef574a67ac44ab23e92f576e3ae8f206c66ab
Alexander Boyko <bass-2000@yandex.ru>
2018-09-05 19:53:59 +0300

    lesson practise

---
+++ stc12-09-sockets/src/main/java/ru/innopolis/stc12/sockets/lesson/Client.java
@@ -0,0 +1,33 @@
+package ru.innopolis.stc12.sockets.lesson;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        Socket socket = null;
+        try {
+            socket = new Socket("127.0.0.1", Server.SERVER_PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            OutputStreamWriter serverOutput = new OutputStreamWriter(socket.getOutputStream());
+            InputStreamReader serverInput = new InputStreamReader(socket.getInputStream());
+            Scanner scanner = new Scanner(System.in);
+            String message;
+            BufferedReader bufferedReader = new BufferedReader(serverInput);
+            while ((message = scanner.nextLine()) != "") {
+                BufferedWriter bufferedWriter = new BufferedWriter(serverOutput);
+                bufferedWriter.write(message);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+
+                System.out.println("Server echo: " + bufferedReader.readLine());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ stc12-09-sockets/src/main/java/ru/innopolis/stc12/sockets/lesson/Server.java
@@ -0,0 +1,28 @@
+package ru.innopolis.stc12.sockets.lesson;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static Integer SERVER_PORT = 4999;
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+            Socket socket = serverSocket.accept();
+            InputStream fromClient = socket.getInputStream();
+            OutputStream toClient = socket.getOutputStream();
+            BufferedReader clientReader = new BufferedReader(new InputStreamReader(fromClient));
+            BufferedWriter clientWriter = new BufferedWriter(new OutputStreamWriter(toClient));
+            String message = null;
+            while ((message = clientReader.readLine()) != null) {
+                System.out.println(message);
+                clientWriter.write("\"" + message + "\" received \n");
+                clientWriter.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
