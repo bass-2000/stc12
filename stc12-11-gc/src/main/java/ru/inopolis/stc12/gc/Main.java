1242ef00f227dcc3c2ef10c75891525f14d3c45d
bass-2000 <bass-2000@yandex.ru>
2018-09-12 09:33:52 +0300

    hw 11 lesson

---
+++ stc12-11-gc/src/main/java/ru/inopolis/stc12/gc/Main.java
@@ -0,0 +1,44 @@
+package ru.inopolis.stc12.gc;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    private static List objects = new ArrayList();
+    private static boolean cont = true;
+    private static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        in.readLine(); //искуственная пауза чтобы открыть в visualvm нужный pid для просмотра
+        while (cont) {
+            System.out.println("Создаем объекты.");
+
+            for (int i = 0; i < 5; i++) {
+                createObjects();
+                Thread.sleep(1000);
+
+            }
+            System.out.println("Удаляем объекты.");
+            for (int i = 0; i < 5; i++) {
+                removeObjects();
+            }
+        }
+    }
+
+    private static void createObjects() {
+        for (int i = 0; i < 2; i++) {
+            objects.add(new byte[1000 * 1000 * 100]);
+        }
+    }
+
+    private static void removeObjects() {
+        int start = objects.size() - 1;
+        int end = start - 2;
+        for (int i = start; ((i >= 0) && (i > end)); i--) {
+            objects.remove(i);
+        }
+    }
+}
