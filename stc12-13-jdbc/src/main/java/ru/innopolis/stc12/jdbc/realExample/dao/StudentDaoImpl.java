d8d2b292ab3a7f953e7e2dd4b33f2259a0b0a778
Alexander Boyko <bass-2000@yandex.ru>
2018-09-19 20:55:58 +0300

    jdbc lesson package

--- stc12-13-jdbc/src/main/java/ru/innopolis/stc12/jdbc/realExample/dao/StudentDaoImpl.java
+++ stc12-13-jdbc/src/main/java/ru/innopolis/stc12/jdbc/realExample/dao/StudentDaoImpl.java
@@ -6,6 +6,7 @@ import ru.innopolis.stc12.jdbc.realExample.pojo.Student;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class StudentDaoImpl implements StudentDao {
@@ -33,16 +34,67 @@ public class StudentDaoImpl implements StudentDao {
 
     @Override
     public Student getStudentById(int id) {
-        return null;
+        Connection connection = connectionManager.getConnection();
+        Student student = null;
+        try (PreparedStatement statement = connection.prepareStatement(
+                "SELECT * from students WHERE id = ?");
+        ) {
+            statement.setInt(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    student = new Student(resultSet.getInt("id"),
+                            resultSet.getString("name"),
+                            resultSet.getString("family_name"),
+                            resultSet.getInt("age"),
+                            resultSet.getString("contact"),
+                            resultSet.getInt("city"));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return student;
     }
 
     @Override
     public boolean update(Student student) {
-        return false;
+        if (student.getId() != 0) {
+            Connection connection = connectionManager.getConnection();
+            try (PreparedStatement statement = connection.prepareStatement(
+                    "UPDATE students SET name=?, family_name=?, age=?, contact=?, city=? WHERE id=?");
+            ) {
+                statement.setString(1, student.getName());
+                statement.setString(2, student.getFamily_name());
+                statement.setInt(3, student.getAge());
+                statement.setString(4, student.getContact());
+                statement.setInt(5, student.getCity());
+                statement.setInt(6, student.getId());
+                statement.execute();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return false;
+            }
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public boolean deleteStudentById(int id) {
-        return false;
+        Connection connection = connectionManager.getConnection();
+        Student student = null;
+        try (PreparedStatement statement = connection.prepareStatement(
+                "DELETE FROM students WHERE id=?");
+        ) {
+            statement.setInt(1, id);
+            statement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
+
 }
