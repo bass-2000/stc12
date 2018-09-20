07673776651d867b273e404612302f707da8ab33
bass-2000 <bass-2000@yandex.ru>
2018-09-20 10:21:26 +0300

    fix main.class

--- stc12-13-jdbc/src/main/java/ru/innopolis/stc12/jdbc/realExample/Main.java
+++ stc12-13-jdbc/src/main/java/ru/innopolis/stc12/jdbc/realExample/Main.java
@@ -1,13 +1,15 @@
 package ru.innopolis.stc12.jdbc.realExample;
 
 import ru.innopolis.stc12.jdbc.realExample.dao.StudentDaoImpl;
+import ru.innopolis.stc12.jdbc.realExample.pojo.City;
 import ru.innopolis.stc12.jdbc.realExample.pojo.Student;
 
 public class Main {
 
     public static void main(String[] args) {
         StudentDaoImpl studentDao = new StudentDaoImpl();
-        Student student = new Student(0, "Alexander", "Pushkin", 26, "jgdgjs", 4);
+        City city = new City(0, "Kazan", 75000);
+        Student student = new Student(0, "Alexander", "Pushkin", 26, "jgdgjs", city);
         studentDao.addStudent(student);
     }
 }
