6bd760473c61324b598f8d10575fdf4f0aff3aa0
bass-2000 <bass-2000@yandex.ru>
2018-09-27 16:45:34 +0300

    hw7 fix by commentaries

--- stc12-07-proxy/src/main/java/ru/innopolis/stc12/proxy/MathBoxInvocationHandler.java
+++ stc12-07-proxy/src/main/java/ru/innopolis/stc12/proxy/MathBoxInvocationHandler.java
@@ -5,10 +5,10 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.util.TreeSet;
 
-public class MathBoxInvocationHandler<Integer> implements InvocationHandler {
-    MathBoxInterface<Integer> mathB;
+public class MathBoxInvocationHandler<T> implements InvocationHandler {
+    MathBoxInterface<T> mathB;
 
-    public MathBoxInvocationHandler(MathBoxInterface<Integer> mathBox) {
+    public MathBoxInvocationHandler(MathBoxInterface<T> mathBox) {
         mathB = mathBox;
     }
 
@@ -19,10 +19,10 @@ public class MathBoxInvocationHandler<Integer> implements InvocationHandler {
         }
         Method[] methods = mathB.getClass().getMethods();
         for (Method m : methods) {
-            if (method.getName().equals(m.getName()) && m.getAnnotation(ClearData.class) != null) {
+            if (method.getName().equals(m.getName()) && (method.getParameters().getClass().equals(m.getParameters().getClass())) && m.getAnnotation(ClearData.class) != null) {
                 Field enterSet = mathB.getClass().getDeclaredField("enterSet");
                 enterSet.setAccessible(true);
-                enterSet.set(mathB, new TreeSet<Integer>());
+                enterSet.set(mathB, new TreeSet<T>());
                 System.out.println("Data was cleared");
             }
 
