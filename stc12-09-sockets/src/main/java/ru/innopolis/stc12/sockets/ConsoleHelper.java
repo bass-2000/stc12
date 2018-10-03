2a1ea6b5f8f862c0b21ee7cc7e120094967c3d7d
bass-2000 <bass-2000@yandex.ru>
2018-10-03 10:30:09 +0300

    added logger

--- stc12-09-sockets/src/main/java/ru/innopolis/stc12/sockets/ConsoleHelper.java
+++ stc12-09-sockets/src/main/java/ru/innopolis/stc12/sockets/ConsoleHelper.java
@@ -1,15 +1,18 @@
 package ru.innopolis.stc12.sockets;
 
+import org.apache.log4j.Logger;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ConsoleHelper {
+    private static Logger logger = Logger.getLogger(ConsoleHelper.class);
     private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
 
     public static void writeMessage(String message) {
-        System.out.println(message);
+        logger.info(message);
     }
 
     public static String readString() {
