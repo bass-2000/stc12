915851151ef387b26b04013536f2a02375f81ae5
bass-2000 <bass-2000@yandex.ru>
2018-10-05 12:38:06 +0300

    was a mistake

--- stc12-15-testing/src/main/java/ru/innopolis/stc12/testing/third/WordFinderUser.java
+++ stc12-15-testing/src/main/java/ru/innopolis/stc12/testing/third/WordFinderUser.java
@@ -2,7 +2,7 @@ package ru.innopolis.stc12.testing.third;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashSet;
+import java.util.Set;
 
 class WordFinderUser {
     private WordFinder wordFinder;
@@ -12,7 +12,7 @@ class WordFinderUser {
     }
 
     void doWork(String resource, String word) throws MalformedURLException {
-        HashSet<String> sentences = wordFinder.getSentences(new URL(resource));
+        Set<String> sentences = wordFinder.getSentences(new URL(resource));
         for (String sentence : sentences) {
             if (wordFinder.checkInWordInSentence(word, sentence)) wordFinder.writeSentenceToResult(sentence);
         }
